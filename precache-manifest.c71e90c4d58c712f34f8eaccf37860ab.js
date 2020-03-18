1f2e618327a5fff9452941efa80e30ed4dfcff4a
Armando Velazquez <davidarmando.gdl@live.com>
2020-03-18 00:14:03 -0600

    Updates

---
+++ precache-manifest.c71e90c4d58c712f34f8eaccf37860ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89a03445ebe8e4cd4e6954545e7f868",
+    "url": "/steps/index.html"
+  },
+  {
+    "revision": "b32296147c12d1471522",
+    "url": "/steps/static/css/main.2a758061.chunk.css"
+  },
+  {
+    "revision": "c3797982d5a80354d8ae",
+    "url": "/steps/static/js/2.809d5ff5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/steps/static/js/2.809d5ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32296147c12d1471522",
+    "url": "/steps/static/js/main.6d56e89b.chunk.js"
+  },
+  {
+    "revision": "b486ea46f77a14d49ba4",
+    "url": "/steps/static/js/runtime-main.eae59cbb.js"
+  }
+]);
